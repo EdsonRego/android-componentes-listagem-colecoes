049cb782c006b2f2f576e7368eb66d3d30e79ea0
Edson Gomes do Rego <edsonxrego@gmail.com>
2026-09-20 12:06:49 -0300

    Encadeamento de métodos

--- app/src/main/java/com/edsonrego/aulacomponenteslistagemcolecoes/MainActivity.kt
+++ app/src/main/java/com/edsonrego/aulacomponenteslistagemcolecoes/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -36,7 +37,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickBotao(view: View){
+
+        Toast
+            .makeText(
+                this,
+                "Sucesso ao fazer algo",
+                Toast.LENGTH_SHORT
+            ).show()
+
         textResultado.text = "Olá Mundo 2"
     }
+    
 
 }
---
+++ app/src/main/java/com/edsonrego/aulacomponenteslistagemcolecoes/testes/Encadeamento.kt
@@ -0,0 +1,30 @@
+package com.edsonrego.aulacomponenteslistagemcolecoes.testes
+
+class Mensagem(
+    val mensagem: String,
+    val duracao: Int
+){
+
+    companion object{
+
+        const val DURACAO_CURTA = 0
+        const val DURACAO_LONGA = 1
+
+        fun construirTexto(mensagem: String, duracao: Int): Mensagem{
+            return Mensagem(mensagem,duracao)
+        }
+    }
+
+    fun exibir(){
+        println("M: ${this.mensagem} - D: ${this.duracao}")
+    }
+}
+
+fun main() {
+
+    Mensagem.construirTexto(
+        "Olá Mundo",
+        Mensagem.DURACAO_LONGA
+    ).exibir()
+
+}
